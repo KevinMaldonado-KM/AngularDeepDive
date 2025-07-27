d09b0cec7f5cac3864d778e3abf276a000d94250
Kévin MALDONADO <kevin.maldonado@etu.univ-amu.fr>
2025-07-27 19:43:43 +0200

    Accès aux éléments de templates grâce à viewChild (SECTION 6 - COURS 132 DONE)

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild, ViewChildren } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
+  onSubmit(title: string, ticketText: String){
+    console.log(title);
+    console.log(ticketText);
+    this.form().nativeElement.reset();
+  }
 }
